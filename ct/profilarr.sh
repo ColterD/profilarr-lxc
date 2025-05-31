24fa151c404f50620aa5b36c01603bfce49fce36
Colter Dahlberg <29168599+ColterD@users.noreply.github.com>
2025-05-30 22:32:35 -0500

    Update profilarr.sh

--- ct/profilarr.sh
+++ ct/profilarr.sh
@@ -20,6 +20,7 @@ var_disk="${var_disk:-2}"
 var_os="${var_os:-debian}"
 var_version="${var_version:-12}"
 var_unprivileged="${var_unprivileged:-1}"
+var_port="6868"
 
 header_info "$APP"
 variables
@@ -47,9 +48,10 @@ function update_script() {
 
 start
 build_container
+
 description
 
 msg_ok "Completed Successfully!\n"
 echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
 echo -e "${INFO}${YW} Access it using the following IP:${CL}"
-echo -e "${TAB}${GATEWAY}${BGN}${IP}${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}${IP}:${var_port}${CL}"
